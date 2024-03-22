8ac0b46e17e9d910ae05e4989fe98cd769382e34
Samir Arman <samir.arman@gmail.com>
2024-03-22 12:33:09 -0300

    Update scrapper_minimal.py

--- scrapper_minimal.py
+++ scrapper_minimal.py
@@ -4,6 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
 import time
 from datetime import date
 import sys
@@ -20,23 +25,26 @@ def initial_date():
 def final_date():
     return date.today().strftime("%d/%m/%Y")
 
-firefox_options = Options()
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
 options = [
     "--headless",
     "--disable-gpu",
-    "--width=2560",
-    "--height=1920",
+    "--window-size=1920,1200",
     "--ignore-certificate-errors",
     "--disable-extensions",
     "--no-sandbox",
     "--disable-dev-shm-usage"
 ]
 for option in options:
-    firefox_options.add_argument(option)
+    chrome_options.add_argument(option)
 
-print("Starting Firefox")
-driver = webdriver.Firefox(options=firefox_options)
-driver.implicitly_wait(WAIT)
+print("Starting Chrome")
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+
+
 
 print("Firefox started. Navigating to a page")
 driver.get("https://www.google.com/")
